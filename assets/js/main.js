108ce74a8a803fa8f9bdbf9e29e7dec652f2ee00
pollohultima <matteo.polonio@gmail.com>
2021-10-22 14:59:32 +0200

    Initial and final commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const ultima =
+    [
+        {
+            'nickname': 'Pollo',
+            'role': 'bass_player',
+            'age': '27'
+        },
+
+        {
+            'nickname': 'Trizio',
+            'role': 'lead_guitar_player',
+            'age': '25'
+        },
+
+        {
+            'nickname': 'Massi',
+            'role': 'rythm_guitar_player',
+            'age': '30'
+        },
+
+        {
+            'nickname': 'Ceres',
+            'role': 'drummer',
+            'age': '28'
+        },
+
+        {
+            'nickname': 'Toffa',
+            'role': 'vocalist',
+            'age': '22'
+        },
+    ];
+
+console.log(ultima);
